7e8c6065c8bb17a19626ff0c28ab3808c758d182
Ashley Moore <mooream2291@gmail.com>
2021-01-18 23:07:01 -0500

    not complete

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from'react';
+
+import ToDo from './components/todo/todo.js';
+
+function App(props) {
+    // const [] = useState();
+    return (
+      <>
+        <ToDo />
+      </>
+    );
+  }
+
+export default App;
